77ad8adc424b8ec74597b6a7ceea501d95ed3ddf
Cancey Dejean <canceyd@gmail.com>
2024-12-09 12:20:08 -0600

    Enhance NavLink component with new features and styling improvements
    - Added support for opening links in a new tab via the `newTab` prop.
    - Refactored the className handling using the `cn` utility for better readability and maintainability.
    - Included an icon (`SquareArrowOutUpRight`) to visually indicate external links.
    - Improved overall styling consistency and responsiveness of the NavLink component.

--- src/components/ui/Header/_components/NavLink.tsx
+++ src/components/ui/Header/_components/NavLink.tsx
@@ -1,4 +1,6 @@
+import { cn } from "@/lib/utils";
 import { LinkItem } from "@/types";
+import { SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
 
 type NavLinkProps = LinkItem & {
@@ -11,6 +13,7 @@ export default function NavLink({
   id,
   label,
   url,
+  newTab,
   className,
   onlyMobile,
   handleClick,
@@ -19,12 +22,17 @@ export default function NavLink({
     <Link
       key={id}
       href={url || "#"}
-      className={`font-code hover:text-primary relative block text-2xl text-white uppercase transition-colors lg:text-white/50 ${
-        onlyMobile ? "lg:hidden" : ""
-      } px-6 py-6 md:py-8 lg:-mr-0.25 lg:px-12 lg:text-xs lg:leading-5 lg:font-semibold lg:hover:text-white ${className || ""}`}
+      className={cn(
+        "font-code hover:text-primary relative inline-flex items-center gap-2 px-6 py-6 text-2xl text-white uppercase transition-colors md:py-8 lg:-mr-0.25 lg:px-12 lg:text-xs lg:leading-5 lg:font-semibold lg:text-white/50 lg:hover:text-white",
+        onlyMobile && "lg:hidden",
+        className,
+      )}
       onClick={handleClick}
+      target={newTab ? "_blank" : undefined}
     >
       {label || "Link"}
+
+      {newTab && <SquareArrowOutUpRight className="size-3" />}
     </Link>
   );
 }
